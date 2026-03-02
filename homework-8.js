9b97625f7bf5a9c22d27659a5580739d37a9c26d
Aziz-Saifuddinov <azizjonsaifuddinov@gmail.com>
2026-03-02 22:20:23 +0500

    error corection

--- homework-8.js
+++ homework-8.js
@@ -3,29 +3,29 @@ import{ productCards } from "./productCards.js";
 // 3. По аналогии из лекции — создать и реализовать шаблон для продуктовых карточек.
 
 const productTemplate = document.getElementById('product-template');
-const productList = document.getElementById('product-list')
+const productList = document.getElementById('product-list');
 
 function outputProductCards(productCards) {
   productCards.forEach(product => {
     const productClone = productTemplate.content.cloneNode(true);
     productClone.querySelector('.product-img').src = `./images/${product.img}.svg`;
-    productClone.querySelector('.product-category').textContent = product.category
-    productClone.querySelector('.product-name').textContent = product.name
-    productClone.querySelector('.product-description').textContent = product.description
+    productClone.querySelector('.product-category').textContent = product.category;
+    productClone.querySelector('.product-name').textContent = product.name;
+    productClone.querySelector('.product-description').textContent = product.description;
     productClone.querySelector('.product-compound').innerHTML = product.compound.map(item => `<li>${item}</li>`).join("");
-    productClone.querySelector('.cost-product').textContent = product.price + " \u20BD"
+    productClone.querySelector('.cost-product').textContent = product.price + " \u20BD";
     productList.appendChild(productClone);
   })
 };
 
 // 4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
 
-const getProductNames = productCards.reduce((accum, productCard) => [...accum, productCard.name], []).join('; ')
+const getProductNames = productCards.reduce((accum, productCard) => [...accum, productCard.name], []).join('; ');
 console.log(getProductNames);
 
 // 5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
 
-const productDescription = productCards.reduce((accum, product) => [...accum, {[product.name]: product.description}], [])
+const productDescription = productCards.reduce((accum, product) => [...accum, {[product.name]: product.description}], []);f
 console.log(productDescription);
 
 // 6*. Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, мол "Сколько карточек отобразить? От 1 до 5"
@@ -40,6 +40,6 @@ const showCards = () => {
   } else {
     alert("Введите число от 1 до 5");
   }
-}
+};
 
 showCards();
